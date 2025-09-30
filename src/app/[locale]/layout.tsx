eb5a39deb7a56a9a0a603e56dc4ff5a5fbd8f728
Virginia-V <varzarivirginia4@gmail.com>
2025-09-30 21:43:52 +0300

    fix mistakes with locale

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,15 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+
+export default async function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const messages = await getMessages();
+  return (
+    <NextIntlClientProvider messages={messages}>
+      {children}
+    </NextIntlClientProvider>
+  );
+}
