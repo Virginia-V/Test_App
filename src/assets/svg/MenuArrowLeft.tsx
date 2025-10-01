b7aa5ee88e6f2cff59657ffa629bf910acffe38d
Virginia-V <varzarivirginia4@gmail.com>
2025-10-01 17:02:57 +0300

    Change the color of arrows

--- src/assets/svg/MenuArrowLeft.tsx
+++ src/assets/svg/MenuArrowLeft.tsx
@@ -1,11 +1,13 @@
 type MenuArrowLeftProps = {
   width?: number;
   height?: number;
+  className?: string;
 };
 
 export const MenuArrowLeft: React.FC<MenuArrowLeftProps> = ({
   width = 70,
-  height = 24
+  height = 24,
+  className
 }) => {
   return (
     <svg
@@ -14,25 +16,29 @@ export const MenuArrowLeft: React.FC<MenuArrowLeftProps> = ({
       height={height}
       viewBox="0 0 70 24"
       fill="none"
+      className={className}
     >
       <g transform="translate(0, 0)">
         <path
           d="M12 0H8L0 11.65L0 12.35L8 24H12L4 12.35L12 0Z"
-          fill="#F5F5F5"
+          // fill="#F5F5F5"
+          fill="#4b2a14"
         />
       </g>
 
       <g transform="translate(6, 0)">
         <path
           d="M12 0H8L0 11.65L0 12.35L8 24H12L4 12.35L12 0Z"
-          fill="#F5F5F5"
+          // fill="#F5F5F5"
+          fill="#4b2a14"
         />
       </g>
 
       <g transform="translate(12, 0)">
         <path
           d="M19 0H9L0 11.65L0 12.35L9 24H19L10 12.35L19 0Z"
-          fill="#F5F5F5"
+          // fill="#F5F5F5"
+          fill="#4b2a14"
         />
       </g>
     </svg>
--- src/assets/svg/MenuArrowRight.tsx
+++ src/assets/svg/MenuArrowRight.tsx
@@ -1,11 +1,13 @@
 type MenuArrowRightProps = {
   width?: number;
   height?: number;
+  className?: string;
 };
 
 export const MenuArrowRight: React.FC<MenuArrowRightProps> = ({
   width = 70,
-  height = 24
+  height = 24,
+  className
 }) => {
   return (
     <svg
@@ -14,23 +16,31 @@ export const MenuArrowRight: React.FC<MenuArrowRightProps> = ({
       height={height}
       viewBox="0 0 70 24"
       fill="none"
+      className={className}
     >
       {/* Thick Arrow (first) */}
       <g transform="translate(0, 0)">
         <path
           d="M0 0H10L19 11.65L19 12.35L10 24H0L9 12.35L0 0Z"
-          fill="#F5F5F5"
+          // fill="#F5F5F5"
+          fill="#4b2a14"
         />
       </g>
 
       {/* Thin Arrow 1 */}
       <g transform="translate(13, 0)">
-        <path d="M0 0H4L12 11.65L12 12.35L4 24H0L8 12.35L0 0Z" fill="#F5F5F5" />
+        <path
+          d="M0 0H4L12 11.65L12 12.35L4 24H0L8 12.35L0 0Z" // fill="#F5F5F5"
+          fill="#4b2a14"
+        />
       </g>
 
       {/* Thin Arrow 2 */}
       <g transform="translate(19, 0)">
-        <path d="M0 0H4L12 11.65L12 12.35L4 24H0L8 12.35L0 0Z" fill="#F5F5F5" />
+        <path
+          d="M0 0H4L12 11.65L12 12.35L4 24H0L8 12.35L0 0Z" // fill="#F5F5F5"
+          fill="#4b2a14"
+        />
       </g>
     </svg>
   );
