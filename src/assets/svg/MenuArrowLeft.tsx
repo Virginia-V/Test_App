c9efb50de6961f8ac5243f136ae9d77ecfa43683
Virginia-V <varzarivirginia4@gmail.com>
2025-10-06 10:35:40 +0300

    Update the colors of the  MenuArrowRight and MenuArrowLeft components

--- src/assets/svg/MenuArrowLeft.tsx
+++ src/assets/svg/MenuArrowLeft.tsx
@@ -21,24 +21,24 @@ export const MenuArrowLeft: React.FC<MenuArrowLeftProps> = ({
       <g transform="translate(0, 0)">
         <path
           d="M12 0H8L0 11.65L0 12.35L8 24H12L4 12.35L12 0Z"
-          // fill="#F5F5F5"
-          fill="#4b2a14"
+          fill="#F5F5F5"
+          // fill="#4b2a14"
         />
       </g>
 
       <g transform="translate(6, 0)">
         <path
           d="M12 0H8L0 11.65L0 12.35L8 24H12L4 12.35L12 0Z"
-          // fill="#F5F5F5"
-          fill="#4b2a14"
+          fill="#F5F5F5"
+          // fill="#4b2a14"
         />
       </g>
 
       <g transform="translate(12, 0)">
         <path
           d="M19 0H9L0 11.65L0 12.35L9 24H19L10 12.35L19 0Z"
-          // fill="#F5F5F5"
-          fill="#4b2a14"
+          fill="#F5F5F5"
+          // fill="#4b2a14"
         />
       </g>
     </svg>
--- src/assets/svg/MenuArrowRight.tsx
+++ src/assets/svg/MenuArrowRight.tsx
@@ -22,24 +22,26 @@ export const MenuArrowRight: React.FC<MenuArrowRightProps> = ({
       <g transform="translate(0, 0)">
         <path
           d="M0 0H10L19 11.65L19 12.35L10 24H0L9 12.35L0 0Z"
-          // fill="#F5F5F5"
-          fill="#4b2a14"
+          fill="#F5F5F5"
+          // fill="#4b2a14"
         />
       </g>
 
       {/* Thin Arrow 1 */}
       <g transform="translate(13, 0)">
         <path
-          d="M0 0H4L12 11.65L12 12.35L4 24H0L8 12.35L0 0Z" // fill="#F5F5F5"
-          fill="#4b2a14"
+          d="M0 0H4L12 11.65L12 12.35L4 24H0L8 12.35L0 0Z"
+          // fill="#4b2a14"
+          fill="#F5F5F5"
         />
       </g>
 
       {/* Thin Arrow 2 */}
       <g transform="translate(19, 0)">
         <path
-          d="M0 0H4L12 11.65L12 12.35L4 24H0L8 12.35L0 0Z" // fill="#F5F5F5"
-          fill="#4b2a14"
+          d="M0 0H4L12 11.65L12 12.35L4 24H0L8 12.35L0 0Z"
+          // fill="#4b2a14"
+          fill="#F5F5F5"
         />
       </g>
     </svg>
